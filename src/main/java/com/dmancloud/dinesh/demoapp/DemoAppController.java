d605d3940460a7de6460d8c52aa4d4472338b3cf
Dinesh Mistry <dinesh@dman.cloud>
2023-04-13 19:22:27 -0400

    updated application

--- src/main/java/com/dmancloud/dinesh/demoapp/DemoAppController.java
+++ src/main/java/com/dmancloud/dinesh/demoapp/DemoAppController.java
@@ -1,13 +1,21 @@
 package com.dmancloud.dinesh.demoapp;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-
-@RestController
+@Controller
 public class DemoAppController {
+
     @GetMapping("/")
-    public String hello() {
-        return "Hello World! This is a sample application to demonstrate an end-2-end DevOps Pipeline";
+    public String hello(Model model) {
+        model.addAttribute("message", "Hello World! This is a sample application to demonstrate an end-2-end DevOps Pipeline");
+        return "index";
+    }
+
+    @GetMapping("/error")
+    public String handleError() {
+        return "error";
     }
 }
+
--- src/main/java/com/dmancloud/dinesh/demoapp/DemoappApplication.java
+++ src/main/java/com/dmancloud/dinesh/demoapp/DemoappApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoappApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoappApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(DemoappApplication.class, args);
+    }
 }
